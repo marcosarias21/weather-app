1fef2eb267db06ccc1dd00cc96b7cc13bcb49844
marcosarias21 <marcosariasolaiz@gmail.com>
2022-09-22 20:53:39 -0300

    agregue el searchbar y boostrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useContext } from 'react';
 import './App.css';
+import { SearchBar } from './components/SearchBar';
+import useFetch from './hooks/customFetch/useFetch';
+import weatherContext from './provider/WeatherContext';
 
 function App() {
+  const { list, cityName, places } = useContext(weatherContext);
+
+  const citiesData = useFetch(`http://api.weatherapi.com/v1/search.json?key=feb6eb809bb34569b51171058222109&q=${list}`);
+  const cityData = useFetch(`http://api.weatherapi.com/v1/current.json?key=feb6eb809bb34569b51171058222109&q=${cityName}`);
+  console.log(cityData.data);
   return (
     <>
+    <SearchBar citiesData={citiesData} places={places} />
     </>
   );
 }
