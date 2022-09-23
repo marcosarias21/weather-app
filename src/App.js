0cb2bc68276143673f1a76fc79ef3f636df4e033
facc77 <ferrerfacundo96@gmail.com>
2022-09-22 21:15:44 -0300

    added backgroundVideo component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import BackgroundVideo from './components/BackgroundVideo/BackgroundVideo.jsx';
 
 function App() {
   return (
     <>
+      <BackgroundVideo />
     </>
   );
 }
