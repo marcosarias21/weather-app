56cbdbcf7f91996d13811086e992c38c2cf828e8
marcosarias21 <marcosariasolaiz@gmail.com>
2022-09-28 16:57:23 -0300

    fixee searchbar y cambie el nombre de card a background data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './App.css';
+import BackgroundData from './components/Cards/BackgroundData';
 import { SearchBar } from './components/SearchBar';
 import useFetch from './hooks/customFetch/useFetch';
 import weatherContext from './provider/WeatherContext';
-import { CardPrede } from './components/Cards';
 
 function App() {
   const {
     list, cityName, places, clima,
   } = useContext(weatherContext);
+  const [dataCity, setDataCity] = useState();
 
   const citiesData = useFetch(`http://api.weatherapi.com/v1/search.json?key=feb6eb809bb34569b51171058222109&q=${list}`);
   const cityData = useFetch(`http://api.weatherapi.com/v1/current.json?key=feb6eb809bb34569b51171058222109&q=${cityName}`);
   const cityClima = useFetch(`http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=${clima}&days=1&aqi=no&alerts=no`);
-  console.log('Tucuman:', cityClima.data);
+  console.log(cityClima);
+
+  useEffect(() => {
+    setDataCity(cityData.data);
+  }, [cityData]);
 
   return (
     <>
-      { !cityClima?.loading
-      && <SearchBar citiesData={citiesData} places={places} cityClima={cityClima} />
-      }
+    <SearchBar citiesData={citiesData} places={places} cityClima={cityClima} />
+    <BackgroundData {...dataCity} />
     </>
   );
 }
