964ab085cac51dce8eb379dbd8a8b3db77ed5bc0
facc77 <ferrerfacundo96@gmail.com>
2022-09-29 23:52:44 -0300

    modified app to pass props to backgroundVideo component

--- src/App.js
+++ src/App.js
@@ -14,18 +14,15 @@ function App() {
 
   const citiesData = useFetch(`http://api.weatherapi.com/v1/search.json?key=feb6eb809bb34569b51171058222109&q=${list}`);
   const cityData = useFetch(`http://api.weatherapi.com/v1/current.json?key=feb6eb809bb34569b51171058222109&q=${cityName}`);
-  const cityClima = useFetch(`http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=${clima}&days=1&aqi=no&alerts=no`);
-  console.log(cityClima);
 
   useEffect(() => {
     setDataCity(cityData.data);
   }, [cityData]);
 
-  console.log(cityData.data);
   return (
     <>
       <BackgroundData {...dataCity} />
-      <BackgroundVideo />
+      {cityData.data && <BackgroundVideo weatherData={cityData.data.current}/>}
       <SearchBar citiesData={citiesData} places={places} />
     </>
   );
