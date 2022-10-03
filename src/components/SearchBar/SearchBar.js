dcccb5e4963e75696a704ae9bc0c5c6e26c301c8
fdimanidimani <fdimani@hotmail.com>
2022-10-02 23:08:53 -0300

    change style SearchBar BackgroundData

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -16,8 +16,11 @@ const SearchBar = ({ citiesData, places }) => {
   return (
     <section className='searchSection d-flex flex-column'>
       <div className='d-flex'>
-        <input onChange={handleChange} type="text" className="form-control w-50" id="exampleInputText" aria-describedby="textHelp" />
-        <button onClick={setDataFound} className='btn btn-secondary'>Buscar</button>
+        <input onChange={handleChange}
+               type="text"
+               className="form-control w-60 ml-3 mt-3 mb-3"
+               placeholder='Clima en...' />
+        <button onClick={setDataFound} className='btn btn-secondary mt-3 mb-3' id='button'>Buscar</button>
       </div>
       <div className='background-listPlaces-style'>
         {places?.map(place => (
